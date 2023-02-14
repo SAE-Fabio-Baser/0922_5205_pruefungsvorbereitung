5839627e486da2d8137134a0a08673747d0d293a
Fabio Baser <f.baser@monday-consulting.com>
2023-02-14 12:07:52 +0100

    dockerized backend

--- backend/server.js
+++ backend/server.js
@@ -5,25 +5,25 @@ import { MongoClient } from "mongodb";
 const app = express();
 app.use(cors());
 const PORT = 3000;
-const CONNSTR = "mongodb://root:example@localhost:27017/?authMechanism=DEFAULT";
+const CONNSTR = "mongodb://root:example@mongo:27017/?authMechanism=DEFAULT";
 
 const mongo = new MongoClient(CONNSTR);
 
-const setupExpress = function() {
-    app.get("/posts", async (_req, res) => {
-        const coll = mongo.db("sae").collection("posts");
+const setupExpress = function () {
+  app.get("/posts", async (_req, res) => {
+    const coll = mongo.db("sae").collection("posts");
 
-        try {
-            const posts = await coll.find({}).toArray();
-            res.send({ success: true, data: posts });
-        } catch (err) {
-            res.send({ success: false, error: err });
-        }
-    });
+    try {
+      const posts = await coll.find({}).toArray();
+      res.send({ success: true, data: posts });
+    } catch (err) {
+      res.send({ success: false, error: err });
+    }
+  });
 
-    app.listen(PORT, () => {
-        console.log("Server läuft auf http://localhost:" + PORT);
-    });
+  app.listen(PORT, () => {
+    console.log("Server läuft auf http://localhost:" + PORT);
+  });
 };
 
 mongo.connect().then(setupExpress).catch(console.error);
